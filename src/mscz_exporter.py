c9d3bb39a7acd90c5565b817f497350179caa41e
Alejandro SM <73649874+sm-alejandro@users.noreply.github.com>
2026-03-27 11:20:54 +0100

    Add musescore exporter

---
+++ src/mscz_exporter.py
@@ -0,0 +1,26 @@
+import subprocess
+import sys
+from pathlib import Path
+
+from backend.config import MUSESCORE_EXE
+
+
+def mscz2xml(filepath: Path):
+    "Converts musescore sheet to xml"
+    musicxml_file = filepath.with_suffix(".musicxml")
+    subprocess.run([MUSESCORE_EXE, filepath, "-o", musicxml_file], check=True)
+    return musicxml_file
+
+
+def mscz2pdf(filepath: Path):
+    "Converts musescore sheet to pdf"
+    pdf_file = filepath.with_suffix(".pdf")
+    subprocess.run(
+        [MUSESCORE_EXE, filepath, "-o", pdf_file],
+        check=True,
+    )
+
+
+if __name__ == "__main__":
+    mscz2pdf(Path(sys.argv[1]))
+    mscz2xml(Path(sys.argv[1]))
