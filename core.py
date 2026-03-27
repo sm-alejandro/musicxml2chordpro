c531d99424fd98788ccdab2b747dc94d18ce57e7
Alejandro SM <73649874+sm-alejandro@users.noreply.github.com>
2026-03-27 23:31:52 +0100

    Add directory creatioin for output files

--- core.py
+++ core.py
@@ -12,6 +12,7 @@ load_dotenv()
 
 def mscz2musicxml(input_path: Path, output_path: Path | None):
     output_path = output_path or input_path.with_suffix(".musicxml")
+    output_path.parent.mkdir(parents=True, exist_ok=True)
     subprocess.run(
         [os.getenv("MUSESCORE_EXECUTABLE"), input_path, "-o", output_path], check=True
     )
@@ -30,13 +31,16 @@ def mscz2pdf(input_path: Path, output_path: Path | None):
 
 def musicxml2pro(input_path: Path, output_path: Path | None):
     converter = XML2Pro()
+    output_path.parent.mkdir(parents=True, exist_ok=True)
     output_path = converter.convert_file(input_path, output_path)
     return output_path
 
 
 def pro2pdf(input_path: Path, output_path: Path | None):
+    output_path = output_path or input_path.with_suffix(".pdf")
+    output_path.parent.mkdir(parents=True, exist_ok=True)
     subprocess.run(
-        ["chordpro", input_path, "-o", output_path or input_path.with_suffix(".pdf")],
+        ["chordpro", input_path, "-o", output_path],
         check=False,
     )
 
@@ -44,6 +48,6 @@ def pro2pdf(input_path: Path, output_path: Path | None):
 if __name__ == "__main__":
     mscz2musicxml(Path(sys.argv[1]), None)
     mscz2pdf(Path(sys.argv[1]), None)
-    pro: Path = xml2pro(Path(sys.argv[1]), None)
+    pro: Path = musicxml2pro(Path(sys.argv[1]), None)
     pro2pdf(pro, None)
     pro2pdf(pro, None)
